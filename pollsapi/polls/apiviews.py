4d484a76cfedf0a7438ce519471588022d175e88
Joy <jooy@interia.pl>
2020-06-15 21:10:08 +0200

    api views

---
+++ pollsapi/polls/apiviews.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+class CreateVote(generics.ListCreateAPIView):
+    serializer_class = VoteSerializer
